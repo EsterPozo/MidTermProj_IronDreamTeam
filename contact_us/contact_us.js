06da360c9833f2079d357b1de43b858cd09715a0
EsterPozo <pozo.estherr@gmail.com>
2022-11-28 12:06:56 +0100

    contact us.js add: red if required field empty

--- contact_us/contact_us.js
+++ contact_us/contact_us.js
@@ -1,13 +1,25 @@
 const contSection = document.querySelector('#contact-section');
 const submitBtn = contSection.querySelector('button');
 
+
 submitBtn.addEventListener("click", function(e) {
     const required = contSection.querySelectorAll('input[required]');
-
-    required.forEach(function (element) {
-        if (element.value === "") {
-            element.style.backgroundColor = "red";
-        }
-    })
-
+    //console.log(required);
+    
+    const input2 = contSection.querySelectorAll('input');
+    //console.log(input2[0]);
+    
+   input2.forEach(function (element) {
+    if (element.required) {
+        console.log(element);
+        element.style.border = "3px solid #FF0000";
+        
+        const div2 =  element.parentNode;
+        const label2 = div2.querySelector('label');
+        label2.style.color = "red";
+       
+        //cómo poner rojo el placeholder?
+    }
+   })
+ 
 })
